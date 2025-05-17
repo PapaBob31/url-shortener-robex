e4c2a1b589c8091c31e8ca17d1a3b57e7defe90f
Adedamola Egbebi <egbebitimi121@gmail.com>
2025-05-17 18:29:34 +0100

    Complete implementation of the link-shortening feature

--- src/app/shortening-form/shortening-form.component.ts
+++ src/app/shortening-form/shortening-form.component.ts
@@ -12,42 +12,76 @@ import { provideHttpClient, HttpClient } from '@angular/common/http';
 @Injectable({providedIn: 'root'})
 export class ShorteningFormComponent {
   private http = inject(HttpClient);
-  inputError = false
-  shortenedResults: {originalUrl: string, newUrl: string}[] = []
+  errorOccured = false
+  errorMsg = ""
+
+  shortenedResults: {originalUrl: string, newUrl: string, copied: boolean}[] = []
+
   urlForm =  new FormGroup({
     url: new FormControl('')
   })
 
   constructor() {
     this.urlForm.valueChanges.subscribe(changes => {
-      if (changes.url && changes.url.trim()) { // text input present and it's not whitespace
-        this.inputError = false; // hides the input error if it was previously displayed
+      if (changes.url && changes.url.trim()) { // text input present/changed and it's not whitespace
+        this.errorOccured = false; // hides the input error if any was previously displayed
+        this.errorMsg = ""
       }
     })
   }
 
+  // truncates the text parameter to 35 characters if longer
+  trimTextIfTooLong(text: string) {
+    if (text.length > 35) {
+      return text.slice(0, 35) + "..."
+    }else {
+      return text
+    }
+  }
+
+  // form submit event handler
   onSubmit(event: FormDataEvent) {
     event.preventDefault();
-
     if (!this.urlForm.value.url || !this.urlForm.value.url.trim()){ // no text input or it's just whitespace
-      this.inputError = true // display input error
-      return
+      this.errorOccured = true // display input error
+      this.errorMsg = "Please add a link"
+      return;
     }
+    this.errorOccured = false; // hides the input error if any was previously displayed
+    this.errorMsg = ""
+    this.getAndDisplayShortenedUrl(this.urlForm.value.url)
+  }
 
-    const ORIGINAL_URL = this.urlForm.value.url;
-
-    this.http.post<{data: {tiny_url: string}, code: number; errors: string[]}>(
+  getAndDisplayShortenedUrl(originalUrl: string) {
+    this.http.post<{data: {tiny_url: string}, errors: string[]}>(
       "https://api.tinyurl.com/create",
-      {
-        "url": this.urlForm.value.url,
-      },
-      {
-        headers: {'Authorization': `Bearer invalid-key`},
-      }).subscribe(responseBody => {
-        console.log(responseBody.errors)
-        this.shortenedResults.push({originalUrl: ORIGINAL_URL, newUrl: responseBody.data.tiny_url});
-    })
+      {"url": this.urlForm.value.url},
+      // auth token for the api is only hard-coded here to make it easier for my supervisor
+      // to reproduce the dev environment when doing code reviews as this project is a solution to a challenge
+      {headers: {'Authorization': `Bearer tWvqPoxhE4MuPcTYjDZnPvVcIw8aSpSQntIakdqmICeL50dweV0vSbwUdpEL`}}
 
-    
+    ).subscribe({
+      next: (responseBody) => {
+        this.shortenedResults.push({originalUrl: this.trimTextIfTooLong(originalUrl), newUrl: responseBody.data.tiny_url, copied: false});
+      },
+      error: (error) => {
+        this.errorOccured = true
+        if (error.status) { // must be some sort of http error from the api e.g. Invalid auth token (401)
+          this.errorMsg = "Internal Application Error. Please try again!"
+        }else {
+          this.errorMsg = "Something went wrong. please check your internet connection and try again"
+        }
+      }
+    })
+  }
+
+  async copyLink(event: MouseEvent) {
+    const linkId = (event.target as HTMLButtonElement)!.id
+    const shortUrlObj = this.shortenedResults.find((result) => result.newUrl == linkId)!
+    if (shortUrlObj.copied)
+      return;
+    navigator.clipboard.writeText(shortUrlObj.newUrl)
+    .then(() => shortUrlObj.copied = true)
+    .catch(err => console.error(err.message))
   }
 }
