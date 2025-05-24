22c800a2a97c2b56beeef8565c3d18fa10d62528
Adedamola Egbebi <egbebitimi121@gmail.com>
2025-05-24 18:26:53 +0100

    Add capacitor package for converting code to native android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.urlshortener',
+  appName: 'url-shortener',
+  webDir: './dist/url-shortener-week-3/browser'
+};
+
+export default config;
